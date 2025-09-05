2b7236c8eac9e59ea1984b029be28e4adab0d145
Muhammad Talha <123252748+MuhammadTalha-Ai@users.noreply.github.com>
2025-09-05 18:46:07 +0500

    Update settings.py

--- fyp/settings.py
+++ fyp/settings.py
@@ -40,14 +40,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # add this
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    ...
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
